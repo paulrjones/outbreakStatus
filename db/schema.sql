564ba11f105ecbf79df18ed5ccb1d8f593ab131e
EvanErickson <evanserickson@gmail.com>
2020-03-03 14:17:25 -0800

    added the databases and got the nav bar centered

--- db/schema.sql
+++ db/schema.sql
@@ -6,6 +6,19 @@ USE outbreakStatus_db;
 
 CREATE TABLE users (
   userid INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-  username VARCHAR(30) NOT NULL UNIQUE
+  username VARCHAR(30) NOT NULL UNIQUE,
+  wholename VARCHAR(100),
+  gender VARCHAR(10),
+  age_range INT(3),
+  email VARCHAR(100) UNIQUE,
+  country VARCHAR(60)
 );
 
+CREATE TABLE events (
+  id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  email VARCHAR(100),
+  eventname VARCHAR(500),
+  eventlocation VARCHAR(200),
+  datecreated INT,
+  FOREIGN KEY (email) REFERENCES users(email),
+);
