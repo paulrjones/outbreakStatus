f4645bdbcc0e320dd32702deef687abb3b17b709
Jean Li <legacy812@gmail.com>
2020-03-03 14:18:49 -0800

    starting const

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -7,6 +7,9 @@ L_B_TRAVELADVISORY = 'https://www.travel-advisory.info/api?'
 
 
 
+
+
+//Coronavirus 
 const unirest = require("unirest");
 
 const req = unirest("GET", "https://coronavirus-monitor.p.rapidapi.com/coronavirus/cases_by_country.php");
@@ -15,7 +18,6 @@ req.headers({
   "x-rapidapi-host": "coronavirus-monitor.p.rapidapi.com",
   "x-rapidapi-key": "00660ae8c6msh0c7077bcf362203p13d109jsn8904a4da5f5d"
 });
-
 
 req.end(function (res) {
   if (res.error) throw new Error(res.error);
