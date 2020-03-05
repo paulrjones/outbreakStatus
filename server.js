24a041dec70055c98d2b27bf1d593abf40c1fe12
EvanErickson <evanserickson@gmail.com>
2020-03-05 10:40:51 -0800

    made changes

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
+require('dotenv').config()
+
 const express = require('express')
 const { join } = require('path')
 const app = express()
-const axios = require('axios')
 
 app.use(express.static(join(__dirname, 'public')))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
+// app.use(require('./routes'))
 
-app.listen(3000)
-
+require('./config').sync()
+  .then(() => app.listen(process.env.PORT || 3000))
+  .catch(e => console.error(e))
